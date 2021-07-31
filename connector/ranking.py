26f4b5c79f3371d79e9e05249e05d673538b7dd4
jjo <k01066624566@gmail.com>
2021-07-31 21:05:55 +0900

    upgrade ranking_python

--- connector/ranking.py
+++ connector/ranking.py
@@ -3,20 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-with open('connector/db_infor.json')as json_file:
+with open('home_connector/db_infor.json')as json_file:
         json_data=json.load(json_file)
         
 juso_db=pymysql.connect(
-    user='root', 
-    passwd='qazwsxedc9070@', 
-    host='172.30.1.40', 
-    db='users', 
+    user=json_data["user"], 
+    passwd=json_data["password"], 
+    host=json_data["host"], 
+    db=json_data["database"], 
     charset='utf8'
 )
+
+lang={"C":0,"C++":0,"C#":0,"Python":0,"Java":0,"JavaScript":0,"Kotlin":0,"Swift":0,"Html":0}
+top_lang=[[0]*2]*3
+
 cursor=juso_db.cursor(pymysql.cursors.DictCursor)
 sql="select language from poster"
 cursor.execute(sql)
 result=cursor.fetchall()
-#print(result[0]['language'])
 for a in result:
-    print(a['language'])
+    lang[a['language']]+=1
